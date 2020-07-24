3458f6504d88e5afd7042d1129470dcfceb8d7a6
Vinay Madenahally Prasad <vinaymp25@gmail.com>
2020-07-24 18:03:43 +0530

    Dependency Injection: Setter Injection

--- src/com/kempa/springdemo/CricketCoach.java
+++ src/com/kempa/springdemo/CricketCoach.java
@@ -1,6 +1,14 @@
 package com.kempa.springdemo;
 
 public class CricketCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	// Create a no-arg constructor
+	public CricketCoach() {
+		// TODO Auto-generated constructor stub
+		System.out.println("CricketCoach: inside no-arg constructor");
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -11,7 +19,12 @@ public class CricketCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Practice fast bowling for 15 minutes: " + fortuneService.getFortune();
+	}
+	
+	public void setFortuneService(FortuneService theFortuneService) {
+		System.out.println("CricketCoach: inside setter method - setFortuneService");
+		this.fortuneService = theFortuneService;
 	}
 
 }
